82996297d01ddbd2bd2a86ecad29bb6c1e9fc76b
pcmin929 <pcmin929@gmail.com>
2024-08-30 15:07:34 +0900

    test

--- main.py
+++ main.py
@@ -1,67 +1,10 @@
-# from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
-# from domain.answer import answer_router
-# from domain.question import question_router
-# from fastapi.responses import PlainTextResponse
-# app = FastAPI()
-
-# origins = [
-#         "http://127.0.0.1:5173", "http://localhost:5173" , 
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-# app.include_router(question_router.router)
-# app.include_router(answer_router.router)
-
-# @app.get("/health")
-# async def health_check():
-#     return PlainTextResponse("건강합니다",status_code=200)
-
-# from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
-# from domain.answer import answer_router
-# from domain.question import question_router
-# from fastapi.responses import PlainTextResponse
-
-# app = FastAPI()
-
-# origins = [
-#     "http://127.0.0.1:5173", 
-#     "http://localhost:5173", 
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(question_router.router)
-# app.include_router(answer_router.router)
-
-# @app.get("/health")
-# async def health_check():
-#     return PlainTextResponse("건강합니다", status_code=200)
-
 import os
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from alembic import command
 from alembic.config import Config
-from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-import models
 from domain.answer import answer_router
 from domain.question import question_router
 
@@ -113,6 +56,6 @@ async def health_check():
 async def on_startup():
     run_migrations()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
